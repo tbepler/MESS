972faca030547d1b155be9e66893dbb23a584433
tbepler <tbepler@gmail.com>
2014-11-17 18:43:46 -0500

    PWM reads and scores correctly. Occurrences of motif counted in sequence appear to be correct

--- PositionWeightMatrix.py
+++ PositionWeightMatrix.py
@@ -1,41 +1,65 @@
 import math
+from collections import Counter
 
 
 class PositionWeightMatrix:
-	def __init__(self,l):
-		self.struct = l
-	def asList(self):
-		return self.struct
+	def __init__(self,scores,length):
+		self._struct = scores
+		self._n = length
+		self._alphabet = scores.keys()
+		self._alphabet.sort()
 	def __eq__(self,other):
-		return pwmEq(self.struct, other.struct, 0)
+		return pwmEq(self._struct, other._struct, 0)
 	def __hash__(self):
 		h = 5
-		for i in range(len(alphabet)):
-			for j in range(len(self.struct[i])):
-				h = h*11 + hash(self.struct[i][j])
+		for c in self._alphabet:
+			for j in range(len(self._struct[i])):
+				h = h*11 + hash(self._struct[c][j])
 		return h
 	def __getitem__(self, i):
-		return self.struct[i]
+		return self._struct[i]
 	def length(self):
-		return len(self.struct[0])
+		return self._n
+	def __len__(self):
+		return self._n
 	def score(self, s):
 		assert len(s) == self.length()
-		return sum( [ self.struct[baseIndex[s[i]]][i] for i in range(len(s)) ] )
+		return sum( [ self._struct[s[i]][i] for i in range(len(s)) ] )
+	def scoreAll(self, s):
+		assert len(s) >= self.length()
+		return [ self.score( s[i:i+self.length()] ) for i in range( len(s) - self.length() + 1 ) ]
+			
+	def alphabet(self):
+		return self._alphabet
+	def adjustToBG( self, bg ):
+		return adjustToBG( self, bg )
 	def __str__(self):
-		L = len(self.struct[0])
+		L = self.length()
 		s = "  "
 		for i in range(L):
 			s += "%-5d " % (i+1)
 		s += "\n"
-		for j in range(len(alphabet)):
-			s += " %s" % alphabet[j]
+		for c in self._alphabet:
+			s += " %s" % c
 			for i in range(L):
-				s += " %5.3f" % self.struct[j][i]
+				s += " %5.3f" % self._struct[c][i]
 			s += "\n"
 		return s
 
+def pwmEq(pwm1,pwm2,tolerance):
+    for i in pwm1.keys():
+        if len(pwm1[i]) == len(pwm2[i]):
+            for j in range(len(pwm1[i])):
+                if abs(exp(pwm1[i][j]) - exp(pwm2[i][j])) > tolerance: return False
+        else:
+            return False
+    return True
+
 def adjustToBG(pwm,bg):
-	return PositionWeightMatrix([ [ log(pwm[i][j]/bg[i]) for j in range(len(pwm[i])) ] for i in range(len(alphabet))])
+	return PositionWeightMatrix(
+		{ c : [ pwm[c][j] - log(bg[c]) for j in range(len(pwm)) ] for c in pwm.alphabet() },
+		pwm.length() 
+		)
 
 def log(x):
 	if x == 0: return float("-inf")
@@ -50,12 +74,27 @@ def parseBG( bgF ):
 			counts.update(line.upper().strip())
 	return [ float(counts[x])/float(length) for x in alphabet ]
 
-def nucleotideFrequency( seq ):
+def nucleotideFrequency( seq, pseudocount = 0 ):
 	counts = Counter( seq.upper().strip() )
-	
+	addedCounts = len( counts ) * pseudocount;
+	return { x : float( counts[x] + pseudocount ) / float( len( seq ) + addedCounts ) for x in counts.keys() }
+
+def parseProbabilities( f ):
+	m = {}
+	l = -1
+	for line in f:
+		tokens = line.strip().split()
+		scores = [ log( float( tokens[i] ) ) for i in range( 1, len(tokens ) ) ]
+		if l < 0:
+			l = len( scores )
+		else:
+			assert l == len( scores )
+		m[ tokens[0][0] ] = scores
+	return PositionWeightMatrix( m, l )
 
 def parsePFM( f, pseudocount = 0 ):
 	bases = f.readline().strip().split()
 	pfm = [ { bases[i]:(float(s)+pseudocount) for (i,s) in enumerate(line.strip().split()) } for line in f ]
-	pfm = [ [ pfm[j][alphabet[i]]/float(sum([ pfm[j][k] for k in pfm[j].keys() ])) for j in range(len(pfm)) ] for i in range(len(alphabet)) ]
-	return PositionWeightMatrix(pfm)
+	l = len(pfm)
+	pfm = { c:[ log( pfm[j][c] ) - log( float(sum([ pfm[j][k] for k in pfm[j].keys() ])) ) for j in range(len(pfm)) ] for c in bases }
+	return PositionWeightMatrix(pfm,l)
--- mess.py
+++ mess.py
@@ -1,17 +1,54 @@
 import sys
+import PositionWeightMatrix as PWM
 
+MOTIF_FLAG = '-m'
+SEQ_FLAG = '-s'
+LOG_LIKELIHOOD_FLAG = '-l'
 
+#returns ( [motif files], [Sequence set files], log likelihood cutoff )
+def parseArgs( args ):
+	motifFiles = []
+	seqFiles = []
+	cutoff = 0
+	cur = None
+	i = 0
+	while i < len( args ):
+		if args[i] == MOTIF_FLAG:
+			cur = motifFiles
+		elif args[i] == SEQ_FLAG:
+			cur = seqFiles
+		elif args[i] == LOG_LIKELIHOOD_FLAG:
+			cur = None
+			i += 1
+			cutoff = float( args[i] )	
+		else:
+			cur.append( args[i] )
+		i += 1
+	return motifFiles, seqFiles, cutoff
 
-
-
-
-
-
+def occurrences( pwm, seq, logLikelihoodCutoff ):
+	bg = PWM.nucleotideFrequency( seq )
+	pwmAdj = pwm.adjustToBG( bg )
+	scores = pwmAdj.scoreAll( seq )
+	#print scores
+	occ = 0
+	for s in scores:
+		if s > logLikelihoodCutoff: occ += 1
+	return occ
 
 
 def main( args ):
 	#do stuff
-	a
+	motifFiles, seqFiles, cutoff = parseArgs( args )
+	pwms = [ PWM.parsePFM( open( f, 'r' ) ) for f in motifFiles ]
+	
+	"""
+	testSeq = 'TCGAATATTACGA'
+	for p in pwms:
+		print p
+		print p.scoreAll( testSeq )
+		print occurrences( p, testSeq, 0 )
+	"""	
 
 if __name__ == '__main__':
-	main( sys.argv )
+	main( sys.argv[1:] )
